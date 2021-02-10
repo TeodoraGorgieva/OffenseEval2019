1b1955605c53f63cde79a56f3669efa4eb9fc998
TeodoraGorgieva <68121466+TeodoraGorgieva@users.noreply.github.com>
2021-02-10 11:44:10 +0100

    Update testdata_loader.py

--- testdata_loader.py
+++ testdata_loader.py
@@ -42,22 +42,22 @@ if __name__ == "__main__":
     testdataA['tweet'] = testdataA['tweet'].apply(lambda x: process_tweet(x, **params, trial=False))
     labelsA['label'] = labelsA['label'].apply(lambda x: 0 if x == 'NOT' else 1)
 
-    testsetA = pd.merge(left=testdataA, right=labelsA, left_on='id', right_on='id')
-    testdataA.to_csv('test_taskA.csv', encoding='utf-8')
+    testsetA = pd.merge(testdataA, labelsA, how="inner", on=["id", "id"])
+    testsetA.to_csv('test_taskA.csv', encoding='utf-8')
 
     testdataB['tweet'] = process_tweets(testdataB['tweet'])
     testdataB['tweet'] = testdataA['tweet'].apply(lambda x: process_tweet(x, **params, trial=False))
     labelsB['label'] = labelsB['label'].apply(lambda x: 0 if x == 'UNT' else 1)
 
     testsetB = pd.merge(left=testdataB, right=labelsB, left_on='id', right_on='id')
-    testdataB.to_csv('test_taskB.csv', encoding='utf-8')
+    testsetB.to_csv('test_taskB.csv', encoding='utf-8')
 
     testdataC['tweet'] = process_tweets(testdataC['tweet'])
     testdataC['tweet'] = testdataC['tweet'].apply(lambda x: process_tweet(x, **params, trial=False))
     labelsC['label'] = labelsC.label.map({'IND': 0, 'OTH': 1, 'GRP': 2})
 
     testsetC = pd.merge(left=testdataC, right=labelsC, left_on='id', right_on='id')
-    testdataC.to_csv('test_taskC.csv', encoding='utf-8')
+    testsetC.to_csv('test_taskC.csv', encoding='utf-8')
 
 
 
