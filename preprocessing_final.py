76fd44f8786c19a40ed6e83ed5c86caef0caf411
TeodoraGorgieva <68121466+TeodoraGorgieva@users.noreply.github.com>
2021-02-21 23:34:09 +0100

    Update preprocessing_final.py

--- preprocessing_final.py
+++ preprocessing_final.py
@@ -1,19 +1,14 @@
 import re
-
-import emoji
-import num2words
-import pandas as pd
-from emoji.core import demojize
 from nltk.corpus import stopwords
-from nltk.stem.wordnet import WordNetLemmatizer
+from nltk.tokenize import word_tokenize
 from nltk.tokenize import TweetTokenizer
-from symspellpy.symspellpy import SymSpell
+from nltk.stem.wordnet import WordNetLemmatizer
+from emoji.core import demojize
+import num2words
+import emoji
 
-
-def process_tweets(tweets_column):
-    # tweets_column = tweets_column.apply(lambda x: emoji.demojize(x, delimiters=(" ", " ")))
-
-    return tweets_column
+from symspellpy.symspellpy import SymSpell, Verbosity
+import pandas as pd
 
 
 def process_tweet(tweet,
@@ -23,29 +18,28 @@ def process_tweet(tweet,
                   remove_hashtags=True,
                   appostrophe_handling=True,
                   lemmatize=True,
-                  sym_spell=SymSpell,
                   reduce_lengthenings=True,
                   segment_words=True,
-                  correct_spelling=True,
                   ):
     """
     This function receives tweets and returns clean word-list
     """
-    ### REMOVE HASHTAGS? #####################################################
+    ### Remove hastags #########################
     if remove_hashtags:
         tweet = re.sub(r'#\w+ ?', '', tweet)
 
     ### Handle USERS and URLS ################################################
     if remove_USER_URL:
-        tweet = re.sub(r"@USER", "<user>", tweet)
-        tweet = re.sub(r"URL", "<url>", tweet)
+       tweet = re.sub(r"@USER", "", tweet)
+       tweet = re.sub(r"URL", "<url>", tweet)
+
 
     ### Convert to lower case: Hi->hi, MAGA -> maga ##########################
     tweet = tweet.lower()
 
     ### Cleaning: non-ASCII filtering, some appostrophes, separation #########
     tweet = re.sub(r"’", r"'", tweet)
-    # tweet = re.sub(r"[^A-Za-z0-9'^,!.\/+-=@]", " ", tweet)
+    #tweet = re.sub(r"[^A-Za-z0-9'^,!.\/+-=@]", " ", tweet)
     tweet = re.sub(r",", " ", tweet)
     tweet = re.sub(r"\.", " ", tweet)
     tweet = re.sub(r"!", " ", tweet)
@@ -83,37 +77,29 @@ def process_tweet(tweet,
 
     tweet = re.sub(r"(\d+)", lambda x: num2words.num2words(int(x.group(0))), tweet)
 
-    tweet = emoji.core.demojize(tweet, delimiters=(" ", " "))
+    #tweet = emoji.demojize(tweet, delimiters=(" ", " "))
 
-    # tweet = re.sub(emoji.core.get_emoji_regexp(), r"", tweet)
+    tweet= re.sub(emoji.get_emoji_regexp(), r"", tweet)
 
     ### Remove Punctuation ###################################################
-    # if remove_punctuation:
-    # translator = str.maketrans('', '', ''.join(list(set(string.punctuation) - set("'"))))
-    # tweet = tweet.translate(translator)
+    #if remove_punctuation:
+        #translator = str.maketrans('', '', ''.join(list(set(string.punctuation) - set("'"))))
+        #tweet = tweet.translate(translator)
 
     # Tokenize sentence for further word-level processing
     tokenizer = TweetTokenizer()
     words = tokenizer.tokenize(tweet)
 
     ### Apostrophe handling:    you're   -> you are  ########################
-    APPO = {"aren't": "are not", "can't": "cannot", "couldn't": "could not", "didn't": "did not", "doesn't": "does not",
-            "don't": "do not",
-            "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would", "he'll": "he will",
-            "he's": "he is",
+    APPO = {"aren't": "are not", "can't": "cannot", "couldn't": "could not", "didn't": "did not", "doesn't": "does not", "don't": "do not",
+            "hadn't": "had not", "hasn't": "has not", "haven't": "have not", "he'd": "he would", "he'll": "he will", "he's": "he is",
             "i'd": "I would", "i'll": "I will", "i'm": "I am", "isn't": "is not", "it's": "it is", "it'll": "it will",
-            "i've": "I have", "let's": "let us", "mightn't": "might not", "mustn't": "must not", "shan't": "shall not",
-            "she'd": "she would",
-            "she'll": "she will", "she's": "she is", "shouldn't": "should not", "that's": "that is",
-            "there's": "there is", "they'd": "they would",
-            "they'll": "they will", "they're": "they are", "they've": "they have", "we'd": "we would",
-            "we're": "we are", "weren't": "were not",
-            "we've": "we have", "what'll": "what will", "what're": "what are", "what's": "what is",
-            "what've": "what have", "where's": "where is",
-            "who'd": "who would", "who'll": "who will", "who're": "who are", "who's": "who is", "who've": "who have",
-            "won't": "will not",
-            "wouldn't": "would not", "you'd": "you would", "you'll": "you will", "you're": "you are",
-            "you've": "you have", "'re": " are",
+            "i've": "I have", "let's": "let us", "mightn't": "might not", "mustn't": "must not", "shan't": "shall not", "she'd": "she would",
+            "she'll": "she will", "she's": "she is", "shouldn't": "should not", "that's": "that is", "there's": "there is", "they'd": "they would",
+            "they'll": "they will", "they're": "they are", "they've": "they have", "we'd": "we would", "we're": "we are", "weren't": "were not",
+            "we've": "we have", "what'll": "what will", "what're": "what are", "what's": "what is", "what've": "what have", "where's": "where is",
+            "who'd": "who would", "who'll": "who will", "who're": "who are", "who's": "who is", "who've": "who have", "won't": "will not",
+            "wouldn't": "would not", "you'd": "you would", "you'll": "you will", "you're": "you are", "you've": "you have", "'re": " are",
             "wasn't": "was not", "we'll": " will"}
     if appostrophe_handling:
         words = [APPO[word] if word in APPO else word for word in words]
@@ -121,17 +107,17 @@ def process_tweet(tweet,
     tweet = ' '.join(words)
     words = tokenizer.tokenize(tweet)
 
-    ### Lemmatisation:          drinking -> drink ###########################
+    ### Lemmatisation ###########################
     if lemmatize:
         lemmatizer = WordNetLemmatizer()
         words = [lemmatizer.lemmatize(word, "v") for word in words]
 
-    ### Remove stop words:      is, that, the, ... ##########################
+    ### Remove stop words ##########################
     if remove_stopwords:
         eng_stopwords = set(stopwords.words("english"))
         words = [w for w in words if not w in eng_stopwords]
 
-    ### Reduce lengthening:    aaaaaaaaah -> aah, bleeeeerh -> bleerh #################
+    ### Reduce lengthening #################
     if reduce_lengthenings:
         pattern = re.compile(r"(.)\1{2,}")
         words = [pattern.sub(r"\1\1", w) for w in words]
@@ -150,37 +136,36 @@ params = dict(remove_USER_URL=True,
               lemmatize=True,
               reduce_lengthenings=True,
               segment_words=True,
-              correct_spelling=True,
               remove_hashtags=True,
-              sym_spell=None,
-              )
+   )
 
 
 def setA():
-    data = pd.read_csv("../dataset/olid-training-v1.0.tsv", sep='\t')
+
+    data = pd.read_csv("olid-training-v1.0.tsv", sep='\t')
     data = data[['tweet', 'subtask_a']]
     data['subtask_a'] = data['subtask_a'].apply(lambda x: 0 if x == 'NOT' else 1)
 
     data['tweet'] = data['tweet'].apply(lambda x: process_tweet(x, **params))
 
-    data.to_csv('data/trainA_with_word.csv', encoding='utf-8')
+    data.to_csv('data/trainA2.csv', encoding='utf-8')
 
 
 def setB():
-    data = pd.read_csv("../dataset/olid-training-v1.0.tsv", sep='\t')
+
+    data = pd.read_csv("olid-training-v1.0.tsv", sep='\t')
     data = data[['tweet', 'subtask_b']]
 
-    data['subtask_b'] = data['subtask_b'].fillna("NULL")
-    mapper = {'UNT': 0, 'TIN': 1, 'NULL': 'NULL'}
+    data = data[data['subtask_b'].notna()]
+    mapper = {'UNT': 0, 'TIN': 1}
     data['subtask_b'] = data.subtask_b.map(mapper)
-    data = data.loc[data['subtask_b'] != 'NULL']
 
     data['tweet'] = data['tweet'].apply(lambda x: process_tweet(x, **params))
-    data.to_csv('data/trainB_with_word.csv', encoding='utf-8')
+    data.to_csv('data/trainbdf2.csv', encoding='utf-8')
 
 
 def setC():
-    data = pd.read_csv("../dataset/olid-training-v1.0.tsv", sep='\t')
+    data = pd.read_csv("olid-training-v1.0.tsv", sep='\t')
     data = data[['tweet', 'subtask_c']]
 
     data['subtask_c'] = data['subtask_c'].fillna("NULL")
@@ -189,10 +174,13 @@ def setC():
     data = data.loc[data['subtask_c'] != 'NULL']
 
     data['tweet'] = data['tweet'].apply(lambda x: process_tweet(x, **params))
-    data.to_csv('data/trainC_with_word.csv', encoding='utf-8')
+    data.to_csv('train_taskC_noemoji.csv', encoding='utf-8')
+
 
 
 if __name__ == "__main__":
+
     setA()
     setB()
     setC()
+
