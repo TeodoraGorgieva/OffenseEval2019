5d2f252cfd170071318166c7aafa0a695a6553a2
Angela Taseva <58977520+angelataseva@users.noreply.github.com>
2021-02-23 23:12:38 +0100

    Update deep_learning_models.py

--- deep_learning_models.py
+++ deep_learning_models.py
@@ -1,50 +1,21 @@
-import sys
-import sklearn
-import tensorflow
-import keras
-
-import pandas as pd
-import numpy as np
-import nltk
-from nltk.corpus import stopwords
-from nltk.stem.porter import PorterStemmer
-import re
-import matplotlib.pyplot as plt
-
-# Sci-kit learn
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score
-from sklearn.metrics import classification_report
-
-# Keras
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Sequential
-from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation, Reshape, Convolution2D, Convolution1D, SpatialDropout1D
-from keras.layers.embeddings import Embedding
 from random import shuffle
 
-import os
-import keras.backend as K
-from keras import optimizers
-from keras.callbacks import EarlyStopping, ModelCheckpoint, Callback
-from sklearn.metrics import roc_auc_score, f1_score
-from keras.preprocessing.text import Tokenizer
-from keras.preprocessing.sequence import pad_sequences
-from keras.models import Model, Sequential
+import numpy as np
+import pandas as pd
+from keras.layers import Dense, LSTM, Conv1D, MaxPooling1D, Dropout, Bidirectional, SpatialDropout1D, Flatten
+# Keras
+from keras.layers import Reshape, Convolution2D, Convolution1D
 from keras.layers.embeddings import Embedding
-from keras.layers import Dense, Flatten, LSTM, Conv1D, MaxPooling1D, Dropout, Activation, Bidirectional, concatenate, \
-                         SpatialDropout1D, GlobalAveragePooling1D, GlobalMaxPooling1D, Input, \
-                         Flatten, GRU
+from keras.models import Sequential
+from keras.preprocessing.sequence import pad_sequences
+from keras.preprocessing.text import Tokenizer
+# Sci-kit learn
+from sklearn.model_selection import train_test_split
 
-
-from helper import prepare_embedding_matrix, longest_subsequence, calculate_metrics, confusion_matrix_plot, confusion_matrix
-from imblearn.over_sampling import SMOTE
-from sklearn.utils.class_weight import compute_class_weight
+from helper import prepare_embedding_matrix, calculate_metrics
 
 
 def under_sample(X, y):
-
     idx_0 = np.where(y == 0)[0].tolist()
     idx_1 = np.where(y == 1)[0].tolist()
 
@@ -58,29 +29,24 @@ def under_sample(X, y):
     return X, y
 
 
-def build_LSTM():
+def build_LSTM(vocabulary_size, max_len, embedding_matrix):
+    model_glove = Sequential()
+    model_glove.add(Embedding(vocabulary_size, 200, input_length=max_len, weights=[embedding_matrix], trainable=False))
+    model_glove.add(Dropout(0.5))
+    model_glove.add(Conv1D(64, 5, activation='relu'))
+    model_glove.add(MaxPooling1D(pool_size=4))
+    model_glove.add(LSTM(200))
+    # For sub-classes A and B: activation function='sigmoid' and loss='binary_crossentropy'
+    # For sub-class C: activation function='softmax' and loss='categorical_crossentropy'
+    model_glove.add(Dense(1, activation='softmax'))
+    model_glove.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
+    return model_glove
+
+
+def build_CNN(vocab_length, max_len, X_train):
     model = Sequential()
-    model.add(Embedding(vocabulary_size, 200, input_length=max_len, weights=[embedding_matrix], trainable=False))
-    model.add(Dropout(0.5))
-    model.add(Conv1D(64, 5, activation='relu'))
-    model.add(MaxPooling1D(pool_size=4))
-    model.add(LSTM(200, dropout=0.35, recurrent_dropout=0.35))
-    model.add(SpatialDropout1D(0.5))
-    model.add(Dense(1, activation='sigmoid'))
-    #model.add(Dense(3, activation='softmax'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy', 'f1_macro]'])
-    #model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy', 'f1_macro]'])
-    model.add(Flatten())
-    model.summary()
-
-    return model
-
-
-def build_CNN(vocabulary_size, max_len, embedding_matrix):
-
-    model = Sequential()
-    model.add(Embedding(vocabulary_size, 200, input_length=max_len, weights=[embedding_matrix], trainable=False))
+    model.add(Embedding(vocab_length, 30, input_length=max_len))
     model.add(Convolution1D(64, 5, activation="relu"))
     model.add(Dropout(0.5))
     model.add(Convolution1D(32, 3, activation="relu"))
@@ -88,90 +54,48 @@ def build_CNN(vocabulary_size, max_len, embedding_matrix):
     model.add(Convolution1D(16, 3, activation="sigmoid"))
     model.add(MaxPooling1D(5))
     model.add(Flatten())
-    #model.add(Dense(X_train.shape[1], activation='softmax'))
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+    # model.add(Dense(X_train.shape[1], activation='softmax'))
+    # For sub-classes A and B: activation function='sigmoid' and loss='binary_crossentropy'
+    # For sub-class C: activation function='softmax' and loss='categorical_crossentropy'
+    model.add(Dense(1, activation='softmax'))
+    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
-
     return model
 
 
-def build_biLSTM(EMBEDDING_DIM, vocabulary_size, max_len):
-
+def build_LSTM_CNN(EMBEDDING_DIM, vocabulary_size, max_len):
     model = Sequential()
-    model.add(Embedding(vocabulary_size, 200, input_length=max_len, weights=[embedding_matrix], trainable=False))
-    model.add(SpatialDropout1D(0.4))
-    model.add(Bidirectional(LSTM(EMBEDDING_DIM, return_sequences=True, dropout=0.0, recurrent_dropout=0.0)))
-    model.add(Dense(1, activation='sigmoid'))
-    model.add(Flatten())
-    model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
-    #model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.summary()
-
-    return model
-
-
-def build_LSTM_CNN(EMBEDDING_DIM: object, vocabulary_size: object, max_len: object) -> object:
-
-    model = Sequential()
-    model.add(Embedding(vocabulary_size, 200, input_length=max_len, weights=[embedding_matrix], trainable=False))
+    model.add(Embedding(vocabulary_size + 1, EMBEDDING_DIM, input_length=max_len, trainable=True, name="Embeddings"))
     model.add(SpatialDropout1D(0.4))
     model.add(Dropout(0.4))
-    #model.add(Bidirectional(CuDNNLSTM(RECURRENT_UNITS, return_sequences=True)))
+    # model.add(Bidirectional(CuDNNLSTM(RECURRENT_UNITS, return_sequences=True)))
     model.add(Bidirectional(LSTM(EMBEDDING_DIM, return_sequences=True, dropout=0.0, recurrent_dropout=0.0)))
     model.add(Conv1D(64, kernel_size=2, activation='relu', padding='valid', kernel_initializer='he_uniform'))
     model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.summary()
-
-    return model
-
-
-def build_GRU(EMBEDDING_DIM, vocabulary_size, max_len):
-
-    model = Sequential()
-    model.add(Embedding(vocabulary_size, 200, input_length=max_len, weights=[embedding_matrix], trainable=False))
-    model.add(Bidirectional(LSTM(300, return_sequences = True, dropout=0.35, recurrent_dropout=0.35)))
-    #model.add(Bidirectional(CuDNNLSTM(RECURRENT_UNITS, return_sequences=True)))
-    model.add(GRU(100, dropout=0.35, recurrent_dropout=0.35))
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
-    model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-    model.summary()
-
-    return model
-
-
-def build_biGRU(EMBEDDING_DIM, vocabulary_size, max_len):
-
-    model = Sequential()
-    model.add(Embedding(vocabulary_size, 200, input_length=max_len, weights=[embedding_matrix], trainable=False))
-    model.add(Dropout(0.4))
-    model.add(Bidirectional(GRU(100, dropout=0.35, recurrent_dropout=0.35)))
-    model.add(Flatten())
-    model.add(Dense(1, activation='sigmoid'))
+    # For sub-classes A and B: activation function='sigmoid' and loss='binary_crossentropy'
+    # For sub-class C: activation function='softmax' and loss='categorical_crossentropy'
+    model.add(Dense(1, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 
 
 def build_CNN_LSTM(EMBEDDING_DIM, vocabulary_size, max_len):
-
     model = Sequential()
     model.add(Embedding(vocabulary_size + 1, EMBEDDING_DIM, input_length=max_len, trainable=True, name="Embeddings"))
     model.add(SpatialDropout1D(0.4))
     model.add(Conv1D(64, 4, activation='relu', kernel_initializer='he_uniform'))
     model.add(MaxPooling1D(pool_size=4))
     model.add(Bidirectional(LSTM(100, dropout=0.0, recurrent_dropout=0.0)))
-    model.add(Dense(1, activation='sigmoid'))
+    # For sub-classes A and B: activation function='sigmoid' and loss='binary_crossentropy'
+    # For sub-class C: activation function='softmax' and loss='categorical_crossentropy'
+    model.add(Dense(1, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
 
     return model
 
 
 def build_CNN_2D(vocab_length, max_len, X_train):
-
     model = Sequential()
     model.add(Embedding(vocab_length, 30, input_length=max_len))
     model.add(Reshape((30, max_len, 1)))
@@ -182,66 +106,58 @@ def build_CNN_2D(vocab_length, max_len, X_train):
     model.add(Convolution2D(16, (2, 2), activation="relu"))
     model.add(Dropout(0.7))
     model.add(Flatten())
-    #model.add(Dense(X_train.shape[1], activation='softmax'))
-    model.add(Dense(1, activation='sigmoid'))
+    # model.add(Dense(X_train.shape[1], activation='softmax'))
+    # For sub-classes A and B: activation function='sigmoid' and loss='binary_crossentropy'
+    # For sub-class C: activation function='softmax' and loss='categorical_crossentropy'
+    model.add(Dense(1, activation='softmax'))
     model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
     model.summary()
 
     return model
 
 
-def fit_model(model, X_train, y_train, split, epochs, class_weight):
-
-    hist = model.fit(X_train,y_train,validation_split=split, epochs=epochs,class_weight=class_weight)
-    return hist
-
-
-
 if __name__ == "__main__":
-
-    data = pd.read_csv("data/trainA.csv")
+    data = pd.read_csv("data/trainC.csv")
 
     # TOKENIZING AND CREATING SEQUENCE 1
     vocabulary_size = 30000
-    max_len = 50
+    max_len = 100
     tokenizer = Tokenizer(num_words=vocabulary_size)
     tokenizer.fit_on_texts(data['tweet'])
 
     sequences = tokenizer.texts_to_sequences(data['tweet'])
     X = pad_sequences(sequences, maxlen=max_len)
-    y = np.array(data['subtask_a'])
+    y = np.array(data['subtask_c'])
 
     y = y.reshape(-1, 1)
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0, stratify=y)
-
-    class_weights_flag = False
-    SMOTE_flag = False
     # GLOBAL VARIABLES
     EMBEDDING_DIM = 200
-    class_weight = {0: 1, 1: 1}
+    class_weight = {0: 1, 1: 1, 2: 1}
     epochs = 10
     split = 0.2
-
-    num_classes = 2
-
-    if SMOTE_flag == True:
-      sm = SMOTE(random_state=12)
-      X_train, y_train = sm.fit_sample(X_train, y_train)
-
-    if class_weights_flag == True:
-        if num_classes == 2:
-            cw = compute_class_weight("balanced", np.unique(y_train), y_train)
-        elif num_classes  > 2:
-            y_integers = np.argmax(y_train, axis=1)
-            cw = compute_class_weight('balanced', np.unique(y_integers), y_integers)
-        class_weight = dict(enumerate(cw))
-
 
     # GLOVE WORD EMBEDDINGS
     embedding_matrix = prepare_embedding_matrix(vocabulary_size, tokenizer)
 
-    bilstm = build_biLSTM(EMBEDDING_DIM, vocabulary_size, max_len)
-    fit_model(bilstm, X_train, y_train, split, epochs, class_weight)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=split, random_state=0, stratify=y)
 
-    #lstm_cnn_model = build_LSTM_CNN(EMBEDDING_DIM, vocabulary_size, max_len)
+    lstm_model = build_LSTM(vocabulary_size, max_len, embedding_matrix)
+
+    # cnn_model = build_CNN(vocabulary_size, max_len, X_train)
+
+    # cnn_model2 = build_CNN_2D(vocabulary_size, max_len, X_train)
+
+    # cnn_lstm_model = build_CNN_LSTM(EMBEDDING_DIM, vocabulary_size, max_len)
+
+    # lstm_cnn_model = build_LSTM_CNN(EMBEDDING_DIM, vocabulary_size, max_len)
+
+    hist = lstm_model.fit(X_train,
+                          y_train,
+                          validation_split=split,
+                          epochs=epochs,
+                          class_weight=class_weight)
+
+    y_predicted = lstm_model.predict(X_test)
+    # y_predicted = np.where(y_predicted > 0.5, 1, 0)
+    calculate_metrics(y_test, y_predicted)
